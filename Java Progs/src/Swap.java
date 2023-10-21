79c93dc67d72a972eb10f74fd947d5a347a8bfe7
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2023-10-22 00:22:24 +0530

    added two methods for swapping

--- Java Progs/src/Swap.java
+++ Java Progs/src/Swap.java
@@ -1,16 +1,36 @@
 import java.util.Scanner;
 
 public class Swap {
-    public static void main(String[] args) {
+
+    static void SwapWithoutTemp(int a, int b){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter value of a: ");
-        int a = sc.nextInt();
+         a = sc.nextInt();
         System.out.print("Enter value of b: ");
-        int b = sc.nextInt();
+         b = sc.nextInt();
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("Value after swapping: a = "+a);
+        System.out.println("Value after swapping: b = "+b);
+
+    }
+
+    static void SwapNums(int a, int b){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+         a = sc.nextInt();
+        System.out.print("Enter value of b: ");
+         b = sc.nextInt();
         int temp = a;
         a=b;
         b=temp;
         System.out.println("Value after swapping: a = "+a);
         System.out.println("Value after swapping: b = "+b);
+    }
+
+    public static void main(String[] args) {
+//       SwapWithoutTemp();
+        SwapNums(5,9);
     }
 }
