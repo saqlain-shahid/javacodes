afc17ec48e690b5cd34637fe732ef7cdcd6a865c
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2023-10-22 00:06:20 +0530

    made some new changes to swap

--- Java Progs/src/Swap.java
+++ Java Progs/src/Swap.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Swap {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
         int a = sc.nextInt();
+        System.out.print("Enter value of b: ");
         int b = sc.nextInt();
         int temp = a;
         a=b;
         b=temp;
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println("Value after swapping: a = "+a);
+        System.out.println("Value after swapping: b = "+b);
     }
 }
